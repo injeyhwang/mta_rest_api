234727b5fd2766dcd89deb4feefd138e05482814
Michael Hwang <michaelinjeyhwang@gmail.com>
2025-04-26 20:22:43 -0400

    added get_db_session singleton for dependency injection

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,12 +1,23 @@
+from sqlmodel import Session
+
+from app.db.database import get_db_session as db_session
 from app.services.mta_realtime import MTAServiceRT
 
 
 mta_rt_service = MTAServiceRT()
 
 
+def get_db_session() -> Session:
+    """
+    A singleton for the database session instance. The session object will be dependency
+    injected into database dependent endpoints.
+    """
+    return db_session
+
+
 def get_mta_rt_service() -> MTAServiceRT:
     """
-    Singleton factory for MTAServiceRT instance. The MTAServiceRT will be dependency injected into
+    A singleton for MTAServiceRT instance. The MTAServiceRT object will be dependency injected into
     feed API routes.
     """
     return mta_rt_service
